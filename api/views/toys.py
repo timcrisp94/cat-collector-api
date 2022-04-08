12fe9f0f3eeb504ec1b7c3ec7ecb8551b52c6b89
Tim Crisp <timothycrisp@gmail.com>
2022-04-08 15:03:33 -0500

    index toys

--- api/views/toys.py
+++ api/views/toys.py
@@ -18,3 +18,7 @@ def create():
   db.session.commit()
   return jsonify(toy.serialize()), 201
 
+@toys.route('/', methods=["GET"])
+def index():
+  toys = Toy.query.all()
+  return jsonify([toy.serialize() for toy in toys]), 201
