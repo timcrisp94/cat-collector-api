171aa02775614f84cfb8b7d4cb01c9ab5e9b7f8c
Tim Crisp <timothycrisp@gmail.com>
2022-04-08 16:05:46 -0500

    toy link controller

--- api/views/cats.py
+++ api/views/cats.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, jsonify, request
 from api.middleware import login_required, read_token
 from api.models.feeding import Feeding
-
 from api.models.db import db
 from api.models.cat import Cat
+from api.models.toy import Toy
+from api.models.toy import Association
 
 cats = Blueprint('cats', 'cats')
 
@@ -28,7 +29,10 @@ def show(id):
   cat = Cat.query.filter_by(id=id).first()
   cat_data = cat.serialize()
   cat_data["fed"] = cat.fed_for_today()
-  return jsonify(cat=cat_data), 200
+  toys = Toy.query.filter(Toy.id.notin_([toy.id for toy in cat.toys])).all()
+  toys=[toy.serialize() for toy in toys]
+
+  return jsonify(cat=cat_data, available_toys=toys), 200
 
 @cats.route('/<id>', methods=["PUT"]) 
 @login_required
@@ -80,4 +84,26 @@ def add_feeding(id):
   cat_data["fed"] = cat.fed_for_today()
 
   return jsonify(cat_data), 201
+
+...
+
+...
+
+@cats.route('/<cat_id>/toys/<toy_id>', methods=["LINK"]) 
+@login_required
+def assoc_toy(cat_id, toy_id):
+  data = { "cat_id": cat_id, "toy_id": toy_id }
+
+  profile = read_token(request)
+  cat = Cat.query.filter_by(id=cat_id).first()
+  
+  if cat.profile_id != profile["id"]:
+    return 'Forbidden', 403
+
+  assoc = Association(**data)
+  db.session.add(assoc)
+  db.session.commit()
+
+  cat = Cat.query.filter_by(id=cat_id).first()
+  return jsonify(cat.serialize()), 201
 
